ba888b1cabdb9818a56bc20f743dc61ffba61e0a
Loland-30 <goranloland5@gmail.com>
2026-05-05 23:25:53 +0200

    Filler

--- SpellStack/Backend/obj/Debug/net10.0/Backend.AssemblyInfo.cs
+++ SpellStack/Backend/obj/Debug/net10.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1c4c78bc40e18da62100ff0a3f97d2594cb744f2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f67bcf638d10f6e36ca0e7855ac9154c64111c2d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
