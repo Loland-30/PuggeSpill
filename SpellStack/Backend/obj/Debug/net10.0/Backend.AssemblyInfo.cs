1c4c78bc40e18da62100ff0a3f97d2594cb744f2
Loland-30 <goranloland5@gmail.com>
2026-05-05 23:16:04 +0200

    Added gitignore

--- SpellStack/Backend/obj/Debug/net10.0/Backend.AssemblyInfo.cs
+++ SpellStack/Backend/obj/Debug/net10.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2c4865c4574701dc19cd8c4820d2605e44a42506")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb4dbea5ca6eb49911e6ab6209b0fffddff7e564")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
